2ef18c717d7a1613d375df96d6c318d80b04d101
Naor <naorzruk@gmail.com>
2018-06-17 19:40:50 +0300

    Updated the <a> tag previously placed to Link

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 import "./App.css";
 
