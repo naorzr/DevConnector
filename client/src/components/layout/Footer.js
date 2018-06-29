65c787eb8e5eb9d625cdcb7517c20bacd6b5f500
Naor <naorzruk@gmail.com>
2018-06-29 12:18:50 +0300

    integrated Redux into the app, added an auth reducer and type checking for the register properties

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,8 +2,15 @@ import React from "react";
 
 export default () => {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Copyright &copy; {new Date().getFullYear()} DevConnector
+    <footer class="page-footer bg-dark text-white mt-5 p-4 text-center ">
+      &copy; {new Date().getFullYear()} DevConnector Copyright:
+      <p>Naor Zruk</p>
+      <a
+        target="_blank"
+        href="https://github.com/naorzr?tab=repositories"
+        class="fab fa-4x fa-github"
+      />
+      <p>Check My Other Projects On Github</p>
     </footer>
   );
 };
